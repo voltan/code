24e5e0f09f11f4836b86a481a63752e1d7784b9d
Hossein Azizabadi <djvoltan@gmail.com>
2014-09-26 13:14:01 +0330

    Create serverinfo.php

---
+++ phpinfo/serverinfo.php
@@ -0,0 +1,12 @@
+<?php
+echo 'version : ' . PHP_VERSION;
+echo '<br />';
+echo 'os : ' . PHP_OS;
+echo '<br />';
+echo 'memory_limit : ' . ini_get( 'memory_limit' );
+echo '<br />';
+echo 'post_max_size : ' . ini_get( 'post_max_size' );
+echo '<br />';
+echo 'upload_max_filesize : ' . ini_get( 'upload_max_filesize' );
+echo '<br />';
+?>
